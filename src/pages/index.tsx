eaa4f788015fc6821a1606d005bd911b849fc528
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2023-01-18 23:32:16 -0300

    início do projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+    <div className={`
+    flex justify-center items-center h-screen
+    bg-gradient-to-r from-green-500 to-blue-500
+    `}>
+      <h1>Hello World!</h1>
+    </div>
+    </>
+  )
+}
