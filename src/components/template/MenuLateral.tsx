5e4659b5f98b3f0591e33a32b1f8f88d026b30ab
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2023-01-19 23:16:24 -0300

    Componente logo

--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,10 +1,18 @@
 import { IconeCasa, IconeAjustes, IconeSino } from "../icons";
 import MenuItem from "./MenuItem";
 import { isPropertySignature } from "typescript";
+import Logo from "./Logo";
 
 export default function MenuLateral() {
     return (
         <aside>
+            <div className={`
+                flex flex-col items-center justify-center
+                bg-gradient-to-r from-indigo-500 to-purple-500
+                h-20 w-20
+            `}>
+                <Logo />
+            </div>
             <ul>
                <MenuItem url="/" texto="Início" icone={IconeCasa}/>
                <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
