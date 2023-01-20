5211b8e1b97ae097f7a38cac1c4febe3cecb4995
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2023-01-19 23:47:21 -0300

    Logo Sair

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -1,26 +1,43 @@
 import Link from "next/link"
 
 interface MenuitemProps {
-    url: string
+    url?: string
     texto: string
     icone: any
+    className?: string
+    onClick?: (evento:any) => void
 }
 
 export default function MenuItem(props: MenuitemProps) {
-    return (
-        <li className={`hover:bg-gray-100`}>
-            <Link legacyBehavior href={props.url}>
-                <a className={`
-                flex flex-col justify-center items-center h-20 w-20
+    function renderizarLink() {
+        return (
+            <a className={`
+                    flex flex-col justify-center items-center 
+                    h-20 w-20  text-gray-600
+                    ${props.className}
                 `}>
-                    {props.icone}
-                    <span className={`
-                        text-xs font-light text-gray-600
+                {props.icone}
+                <span className={`
+                    text-xs font-light
                     `}>
-                        {props.texto}
-                    </span>
-                </a>
-            </Link>
+                    {props.texto}
+                </span>
+            </a>
+        )
+    
+    }
+    return (
+        <li onClick={props.onClick} className={
+            `hover:bg-gray-100
+            cursor-pointer
+            `}>
+            {props.url ? (
+                <Link legacyBehavior href={props.url}>
+                {renderizarLink()}
+                </Link>
+            ) : (
+                renderizarLink()
+                )}
         </li>
     )
 }
