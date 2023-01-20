e98b86cc1307895b4f685601dc339f53fb4ed8bd
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2023-01-19 23:00:12 -0300

    Componente Menu Lateral

--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,7 +1,15 @@
+import { IconeCasa, IconeAjustes, IconeSino } from "../icons";
+import MenuItem from "./MenuItem";
+import { isPropertySignature } from "typescript";
+
 export default function MenuLateral() {
     return (
         <aside>
-            <h1>Menu</h1>
+            <ul>
+               <MenuItem url="/" texto="Início" icone={IconeCasa}/>
+               <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
+               <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino} />
+            </ul>
         </aside>
     )
 }
