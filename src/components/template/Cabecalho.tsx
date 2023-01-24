a470f763108b4a8ee770d7f63432e1ccfcb196a4
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2023-01-24 18:32:43 -0300

    Alternando modo escuro e modo claro

--- src/components/template/Cabecalho.tsx
+++ src/components/template/Cabecalho.tsx
@@ -1,3 +1,5 @@
+import useAppData from "../../data/hook/useAppData"
+import BotaoAlternarTema from "./BotaoAlternarTema"
 import Titulo from "./Titulo"
 
 interface CabecalhoProps {
@@ -5,10 +7,15 @@ interface CabecalhoProps {
     subtitulo: string
 }
 
-export default function Layout(props: CabecalhoProps) {
+export default function Cabecalho(props: CabecalhoProps) {
+    const { tema, alternarTema } = useAppData()
+
     return (
-        <div>
+        <div className={`flex`}>
             <Titulo titulo={props.titulo} subtitulo={props.subtitulo}/>
+            <div className={`flex flex-grow justify-end`}>
+                <BotaoAlternarTema tema={tema} alternarTema={alternarTema}/>
+            </div>
         </div>
     )
 
