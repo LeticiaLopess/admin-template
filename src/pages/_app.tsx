934919c5004f45233de6d64f98df52e52bdeb21c
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2023-01-25 00:06:35 -0300

    Iniciando a Autenticação com Firebase

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import AppContext, { AppProvider } from '../data/context/AppContext'
+import { AuthProvider } from '../data/context/AuthContext'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AppProvider>
-      <Component {...pageProps} />
-    </AppProvider>
+    <AuthProvider>
+      <AppProvider>
+        <Component {...pageProps} />
+      </AppProvider>
+    </AuthProvider>
   )
 }
 
