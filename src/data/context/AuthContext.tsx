d734da5463bbe53f22d3b53b9af11c8f329b0192
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2023-01-25 22:08:09 -0300

    Avatar do Usuário

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -10,25 +10,42 @@ interface AuthContextProps {
 
 const AuthContext = createContext<AuthContextProps>({}) 
 
-//async function usuarioNormalizado(usuarioFirebase: firebase.User): Promise<Usuario> {
-    //const token = await usuarioFirebase.getIdToken()
-    //return {
-        //uid: usuarioFirebase.uid,
-        //nome: usuarioFirebase.displayName,
-        //email: usuarioFirebase.email,
-        //token,
-        //provedor: usuarioFirebase.providerData[0].providerId,
-        //imagemUrl: usuarioFirebase.photoURL
+async function usuarioNormalizado(usuarioFirebase: firebase.User): Promise<Usuario> {
+    const token = await usuarioFirebase.getIdToken()
 
-    //}
-//}
+    if (usuarioFirebase.providerData[0]) {
+        return {
+            uid: usuarioFirebase.uid,
+            nome: usuarioFirebase.displayName || "",
+            email: usuarioFirebase.email || "",
+            token,
+            provedor: usuarioFirebase.providerData[0].providerId,
+            imagemUrl: usuarioFirebase.photoURL || ""
+        }
+        
+        } return {
+            uid: usuarioFirebase.uid,
+            nome: usuarioFirebase.displayName || "",
+            email: usuarioFirebase.email || "",
+            token,
+            provedor:'',
+            imagemUrl: usuarioFirebase.photoURL || ""
+        }
+}
 
 export function AuthProvider(props: any) {
     const [usuario, setUsuario] = useState<Usuario>()
 
     async function loginGoogle() {
-        console.log('Login Google...')
-        route.push('/')
+        const resp = await firebase.auth().signInWithPopup(
+            new firebase.auth.GoogleAuthProvider()
+        )
+
+        if(resp.user?.email) {
+            const usuario = await usuarioNormalizado(resp.user)
+            setUsuario(usuario)
+            route.push('/') 
+        }
     }
 
     return (
